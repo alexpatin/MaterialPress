42bf18d2ee54866b485f394cc65da240f8b5705c
Daniel Hickman <danielhickman9@gmail.com>
2015-03-09 21:34:01 -0700

    First Draft - Footer

--- footer.php
+++ footer.php
@@ -1,6 +1,30 @@
   <div id="footer" class="section">
     <div class="footer-block">
-      <iframe src="http://ghbtns.com/github-btn.html?user=alexpatin&repo=materialpress&type=watch&count=true&size=none" allowtransparency="true" frameborder="0" scrolling="0" width="170" height="30"></iframe>
+      <footer class="page-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Footer Content</h5>
+                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © <?php echo date('Y'); ?> Copyright <?php echo get_bloginfo( name, 'display' ); ?> 
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+          </div>
+        </footer>
     </div>
 
   <?php wp_footer();?>
